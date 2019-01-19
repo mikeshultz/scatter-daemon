a7e1e4155f8d0ea04401eaf515ffbd6c2cb96a3f
Mike Shultz <shultzm@gmail.com>
2019-01-19 16:04:12 -0700

    added safe_slice, is_free_space_availible, and fixed a problem in get_from_first

--- scatter_daemon/common/utils.py
+++ scatter_daemon/common/utils.py
@@ -1,16 +1,22 @@
+""" Generic utility functions """
+import os
 import json
 from eth_utils import to_normalized_address
-from .typing import Iterable, DictOfAny, Any, Dict, PS, StrOrBytes, to_path
+from .typing import Collection, DictOfAny, List, Any, Dict, PS, StrOrBytes, to_path
+from .logging import getLogger
+
+log = getLogger(__name__)
 
 
 def get_from_first(key: str, *ds: DictOfAny) -> Any:
     """ Return a value from the first dict with a value """
-    if len(ds) < 1:
+    if type(ds) != tuple or len(ds) < 1 or not isinstance(ds[0], dict):
         raise ValueError('No dictionaries provided')
 
     for d in ds:
         if key in d:
-            return d[key]
+            if d[key] is not None:
+                return d[key]
     return None
 
 
@@ -30,10 +36,17 @@ def address_in_keys(address: StrOrBytes, d: Dict[str, Any]) -> bool:
     return address in [to_normalized_address(k) for k in d.keys()]
 
 
-def safe_slice(to_slice: Iterable, start: int, length: int) -> Iterable:
+def safe_slice(to_slice: List, start: int, end: int) -> List:
     """ Slice an iterable without throwing an exception even if it's out of bounds """
     iter_len = len(to_slice)
-    if iter_len >= length:
-        return to_slice[start:length]
+    if iter_len >= end:
+        return to_slice[start:end]
     else:
         return to_slice[start:iter_len]
+
+
+def is_free_space_available(at_path: PS, size_bytes: int) -> bool:
+    """ Check if there's at least size_bytes available on disk """
+    at_path = to_path(at_path)
+    res = os.statvfs(at_path)
+    return (res.f_bsize * res.f_bfree) > size_bytes
